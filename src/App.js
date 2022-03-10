7ed8ab9fd6865aa25876dd9de070e1ef2b923bbb
poojadeveloper5821 <poojadeveloper5821@gmail.com>
2022-03-10 20:01:08 +0530

    Daily task update

--- src/App.js
+++ src/App.js
@@ -1,65 +1,85 @@
-import "./App.css";
-// import React, { Component } from "react";
-// import StateEx from "./Components/StateEx";
-import StateEx2 from "./Components/StateEx2";
-import StateEx3 from "./Components/StateEx3";
-import Color from "./Components/Color";
-import Drinks from "./Components/Drinks";
-import Clickme from "./Components/Clickme";
-
-// import Heading from "./Components/Heading";
-// import Para from "./Components/Para";
-// import List from "./Components/List";
-// import Form from "./Components/Form";
-// import Table from "./Components/Table";
-// import Carclass from "./Components/Carclass";
-// import Specification from "./Components/Specification";
-// import Props from "./Components/Props";
-// import Props2 from "./Components/Props2";
-// import Message from "./Components/Message";
-// import Counter from "./Components/Counter";
-// import LifecycleA from "./Components/LifecycleA";
-// import MyComponent from "./Components/MyComponent";
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         {/* {/* <Carclass />
-//       <Specification /> */}
-//         {/* <Props  name="Bruce"  heroname="superman">
-//         <p>This is children props.</p>
-//         </Props>
-//       <Props name="Charlie" heroname="batman"></Props>
-//       <button>Action</button>
-//       <Props name="John" heroname="spiderman"/>
-//       <Props2 name="Tony Stark" heroname="Ironman"/>
-//       <Props2 name="Trixie" heroname="Wonder Women"/>
-//       <Props2 name="Lucifer" heroname="Devil"/>  */}
-//         {/* <Message /> */}
-//         {/* <Counter />
-//         <LifecycleA />
-//        <MyComponent /> */}
-//         {/* <StateEx /> */}
-//       </div>
-//     );
-//   }
-// }
+ import "./App.css";
+ import React, { Component } from "react";
+// // import StateEx from "./Components/StateEx";
+ import StateEx2 from "./Components/StateEx2";
+ import StateEx3 from "./Components/StateEx3";
+ import Color from "./Components/Color";
+ import Drinks from "./Components/Drinks";
+ import Clickme from "./Components/Clickme";
+ import Heading from "./Components/Heading";
+ import Para from "./Components/Para";
+ import List from "./Components/List";
+ import Form from "./Components/Form";
+ import Table from "./Components/Table";
+import CarouselSlide from "./Components/CarouselSlide";
+// // import Carclass from "./Components/Carclass";
+// // import Specification from "./Components/Specification";
+// // import Props from "./Components/Props";
+// // import Props2 from "./Components/Props2";
+// // import Message from "./Components/Message";
+// // import Counter from "./Components/Counter";
+// // import LifecycleA from "./Components/LifecycleA";
+// // import MyComponent from "./Components/MyComponent";
+// // class App extends Component {
+// //   render() {
+// //     return (
+// //       <div className="App">
+// //         {/* {/* <Carclass />
+// //       <Specification /> */}
+// //         {/* <Props  name="Bruce"  heroname="superman">
+// //         <p>This is children props.</p>
+// //         </Props>
+// //       <Props name="Charlie" heroname="batman"></Props>
+// //       <button>Action</button>
+// //       <Props name="John" heroname="spiderman"/>
+// //       <Props2 name="Tony Stark" heroname="Ironman"/>
+// //       <Props2 name="Trixie" heroname="Wonder Women"/>
+// //       <Props2 name="Lucifer" heroname="Devil"/>  */}
+// //         {/* <Message /> */}
+// //         {/* <Counter />
+// //         <LifecycleA />
+// //        <MyComponent /> */}
+// //         {/* <StateEx /> */}
+// //       </div>
+// //     );
+// //   }
+// // }
 
 function App() {
-  return (
+   return (
     <>
-      {/* <Heading />
-      <Para />
-       <List />
-       <Form />
-       <Table /> */}
-      <StateEx2 />
-      <StateEx3 />
-      <Color />
-      <Drinks />
-      <Clickme />
+        <Heading />
+        <Para />
+        <List />
+        <Form />
+        <Table /> 
+        <StateEx2 />
+        <StateEx3 />
+        <Color />
+        <Drinks />
+       <Clickme />
+       <CarouselSlide />
     </>
-  );
-}
+   );
+ }
 
-export default App;
+ export default App;
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
