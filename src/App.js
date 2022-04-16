510fae5e65797c2e0884c7e4b5fd9a21a086098b
pdongre123 <pdongre040@gmail.com>
2022-04-16 17:19:02 +0530

    react-route

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Signup from "./Components/Signup";
 import Error from "./Components/Error";
 import Dashboard from "./Components/Dashboard";
 
-
-
 function App() {
   return (
     <>
@@ -14,70 +12,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path='/dashboard' element={<Dashboard />}/>
-          <Route element={<Error/>} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/*" element={<Error />} />
         </Routes>
       </Router>
-
-    
     </>
   );
 }
 
 export default App;
-
-//import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import LoginReg from "./components/pages/auth/LoginReg";
-// import ResetPassword from "./components/pages/auth/ResetPassword";
-// import SendPasswordResetEmail from "./components/pages/auth/SendPasswordResetEmail";
-// import Contact1 from "./components/pages/Contact1";
-// import Dashboard from "./components/pages/Dashboard";
-// import Home1 from "./components/pages/Home1";
-// import Layout1 from "./components/pages/Layout1";
-
-// function App() {
-//   return (
-//     <>
-//       <BrowserRouter>
-//         <Routes>
-//           <Route path="/" element={<Layout1 />}>
-//             <Route index element={<Home1 />} />
-//             <Route path="contact" element={<Contact1 />} />
-//             <Route path="login" element={<LoginReg />} />
-//             <Route path="sendpasswordresetemail" element={<SendPasswordResetEmail />} />
-//             <Route path="reset" element={<ResetPassword />} />
-//           </Route>
-//           <Route path="/dashboard" element={<Dashboard />} />
-//           <Route path="*" element={<h1>Error 404 Page not found !!</h1>} />
-//         </Routes>
-//       </BrowserRouter>
-//     </>
-//   );
-// }
-
-// export default App;
-
-// // class App extends Component {
-// //   render() {
-// //     return (
-// //       <div className="App">
-// //         {/* {/* <Carclass />
-// //       <Specification /> */}
-// //         {/* <Props  name="Bruce"  heroname="superman">
-// //         <p>This is children props.</p>
-// //         </Props>
-// //       <Props name="Charlie" heroname="batman"></Props>
-// //       <button>Action</button>
-// //       <Props name="John" heroname="spiderman"/>
-// //       <Props2 name="Tony Stark" heroname="Ironman"/>
-// //       <Props2 name="Trixie" heroname="Wonder Women"/>
-// //       <Props2 name="Lucifer" heroname="Devil"/>  */}
-// //         {/* <Message /> */}
-// //         {/* <Counter />
-// //         <LifecycleA />
-// //        <MyComponent /> */}
-// //         {/* <StateEx /> */}
-// //       </div>
-// //     );
-// //   }
-// // }
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,97 +1,172 @@
-import { useState } from "react";
-//import Signup from "./Signup";
+import React, { useState } from "react";
+import {useNavigate} from "react-router-dom";
 
-function Login() {
-  const [username, setUsername] = useState();
+function Login(props) {
+  const [email, setEmail] = useState();
   const [password, setPassword] = useState();
-  const [confpassword, setConfPassword] = useState();
 
   const [allEntry, setAllEntry] = useState([]);
 
+  const navigate=useNavigate();
+  const toDashboard=()=>{
+    navigate('/dashboard',{state:{id: new Date().getTime().toString(), email, password }})
+  }
+
   const submitForm = (e) => {
     e.preventDefault();
-    const newEntry = {
-      username: username,
-      password: password,
-      confpassword: confpassword,
-    };
-    setAllEntry([...allEntry, newEntry]);
-    console.log(allEntry);
-  };
-
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-  const handleConfPasswordChange = (e) => {
-    setConfPassword(e.target.value);
-  };
-  const handleSubmit = (e) => {
-    if (password !== confpassword) {
-      alert("password not correct");
+    if (email && password) {
+      const newEntry = { id: new Date().getTime().toString(), email, password };
+      setAllEntry([...allEntry, newEntry]);
+      console.log(allEntry);
+      setEmail("");
+      setPassword("");
     } else {
-      alert("password enter successfully");
+      alert("please fill the data.");
     }
-    e.preventDefault();
+     
   };
+  
 
   return (
     <>
-      <h1>LoginForm</h1>
-      <form
-        action=""
-        onSubmit={submitForm}
-        // onSubmit={(e) => {
-        //   handleSubmit(e);
-        // }}
-      >
-        <label>UserName:</label>
-        <input
-          type="text"
-          value={username}
-          required
-          onChange={(e) => {
-            handleUsernameChange(e);
-          }}
-        />
-        <label>Password:</label>
-        <input
-          type="password"
-          value={password}
-          required
-          onChange={(e) => {
-            handlePasswordChange(e);
-          }}
-        />
-        <label>Confirm Password:</label>
-        <input
-          type="password"
-          value={confpassword}
-          required
-          onChange={(e) => {
-            handleConfPasswordChange(e);
-          }}
-        />
-        <button onClick={handleSubmit} className="btn" type="submit">
-          Submit
-        </button>{" "}
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        <button onClick={handleSubmit} className="btn" type="submit">
-          Register
-        </button>{" "}
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        {/* <p>
-          <buttontoolbar>
-            <button const path="/Signup">
-              {" "}
-              Register{" "}
-            </button>
-          </buttontoolbar>
-        </p> */}
+      <h2>User Login Details</h2>
+
+      <form action="" onSubmit={submitForm}>
+        <label>
+          Email
+          <input
+            type="text"
+            name="email"
+            autoComplete="off"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            name="password"
+            autoComplete="off"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button onClick={()=>{
+          toDashboard()}}>ShowData</button>
       </form>
+
+      <div>
+        {allEntry.map((currElement) => {
+          const { id, email, password } = currElement;
+          return (
+            <div className="showData" key={id}>
+              <p>{email}</p>
+              <p>{password}</p>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
+
 export default Login;
+
+// import { useState } from "react";
+// //import Signup from "./Signup";
+
+// function Login() {
+//   const [username, setUsername] = useState();
+//   const [password, setPassword] = useState();
+//   const [confpassword, setConfPassword] = useState();
+
+//   // const [allEntry, setAllEntry] = useState([]);
+
+//   // const submitForm = (e) => {
+//   //   e.preventDefault();
+//   //   const newEntry = {
+//   //     username: username,
+//   //     password: password,
+//   //     confpassword: confpassword,
+//   //   };
+//   //   setAllEntry([...allEntry, newEntry]);
+//   //   console.log(allEntry);
+//   // };
+
+//   const handleUsernameChange = (e) => {
+//     setUsername(e.target.value);
+//   };
+//   const handlePasswordChange = (e) => {
+//     setPassword(e.target.value);
+//   };
+//   const handleConfPasswordChange = (e) => {
+//     setConfPassword(e.target.value);
+//   };
+//   const handleSubmit = (e) => {
+//     if (password !== confpassword) {
+//       alert("password not correct");
+//     } else {
+//       alert("Submit form successfully");
+//     }
+//     e.preventDefault();
+//   };
+
+//   return (
+//     <>
+//       <h1>LoginForm</h1>
+//       <form
+//         // action=""
+//         // onSubmit={submitForm}
+//          onSubmit={(e) => {
+//            handleSubmit(e);
+//          }}
+//       >
+//         <label>UserName:</label>
+//         <input
+//           type="text"
+//           value={username}
+//           required
+//           onChange={(e) => {
+//             handleUsernameChange(e);
+//           }}
+//         />
+//         <label>Password:</label>
+//         <input
+//           type="password"
+//           value={password}
+//           required
+//           onChange={(e) => {
+//             handlePasswordChange(e);
+//           }}
+//         />
+//         <label>Confirm Password:</label>
+//         <input
+//           type="password"
+//           value={confpassword}
+//           required
+//           onChange={(e) => {
+//             handleConfPasswordChange(e);
+//           }}
+//         />
+//         <button onClick={handleSubmit} className="btn" type="submit">
+//           Submit
+//         </button>{" "}
+//         &nbsp;&nbsp;&nbsp;&nbsp;
+//         <button onClick={handleSubmit} className="btn" type="submit">
+//           Register
+//         </button>{" "}
+//         &nbsp;&nbsp;&nbsp;&nbsp;
+//         {/* <p>
+//           <buttontoolbar>
+//             <button const path="/Signup">
+//               {" "}
+//               Register{" "}
+//             </button>
+//           </buttontoolbar>
+//         </p> */}
+//       </form>
+//     </>
+//   );
+// }
+// export default Login;
