1893c38cbcef5765e2bd31d805c7f80caf4c3328
pdongre123 <pdongre040@gmail.com>
2022-04-28 19:05:53 +0530

    Single user data

--- src/Components/UserDetails.jsx
+++ src/Components/UserDetails.jsx
@@ -1,69 +1,110 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+//import axios from "axios";
 
-export default class App extends React.Component {
-  state = {
-    isLoading: true,
-    users: [],
-    error: null,
-  };
+const UserDetails = () => {
+  const [data, setData] = useState([]);
 
-  fetchUsers() {
-    // Where we're fetching data from
-    fetch(`https://jsonplaceholder.typicode.com/users`)
-      // We get the API response and receive data in JSON format...
+  const apiGet = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      // ...then we update the users state
-      .then((data) =>
-        this.setState({
-          users: data,
-          isLoading: false,
-        })
-      )
-      // Catch any errors we hit and update the app
-      .catch((error) => this.setState({ error, isLoading: false }));
-  }
-
-  componentDidMount() {
-    this.fetchUsers();
-  }
-
-
-  fetchdata=(id)=>{
-      console.log(id);
-    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((res) => res.json())
       .then((json) => {
           console.log(json);
-        this.setState({
-          fetchdata: json
-        });
-      });
+          setData(json)
+    });
+  };
+
+
+  useEffect(()=>{
+      apiGet();
+  },[]);
+
+
+
+  return (
+    <>
+      <h2 text="center">MY API</h2>
+      <br />
+      <button onClick={apiGet}>Fetch API</button>
+      <br />
+      <prev>{JSON.stringify(data,null,2)}</prev> 
+      {/* <div>
+          <ul>
+              {data.map((item)=>(
+                  <li key={item.id}>{item.title}</li>
+                  
+                  ))}
+          </ul>
+      </div> */}
+      
+    </>
+  );
 };
 
-  render() {
-    const { isLoading, users, error } = this.state;
-    return (
-      <>
-        <h1>Random User</h1>
-        {/* // Display a message if we encounter an error */}
-        {error ? <p>{error.message}</p> : null}
-        {/* // Here's our data check */}
-        {!isLoading ? (
-          users.map((user) => {
-            const { username, name, email } = user;
-            return (
-              <div key={username}>
-                <p>Name: {name}</p>
-                <p>Email Address: {email}</p>
-                <hr />
-              </div>
-            );
-          })
-        ) : (
-          // If there is a delay in data, let's let the user know it's loading
-          <h3>Loading...</h3>
-        )}
-      </>
-    );
-  }
-}
+export default UserDetails;
+
+// export default class App extends React.Component {
+//   state = {
+//     isLoading: true,
+//     users: [],
+//     error: null,
+//   };
+
+//   fetchUsers() {
+//     // Where we're fetching data from
+//     fetch(`https://jsonplaceholder.typicode.com/users`)
+//       // We get the API response and receive data in JSON format...
+//       .then((response) => response.json())
+//       // ...then we update the users state
+//       .then((data) =>
+//         this.setState({
+//           users: data,
+//           isLoading: false,
+//         })
+//       )
+//       // Catch any errors we hit and update the app
+//       .catch((error) => this.setState({ error, isLoading: false }));
+//   }
+
+//   componentDidMount() {
+//     this.fetchUsers();
+//   }
+
+//   fetchdata=(id)=>{
+//       console.log(id);
+//     fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+//       .then((res) => res.json())
+//       .then((json) => {
+//           console.log(json);
+//         this.setState({
+//           fetchdata: json
+//         });
+//       });
+// };
+
+//   render() {
+//     const { isLoading, users, error } = this.state;
+//     return (
+//       <>
+//         <h1>Random User</h1>
+//         {/* // Display a message if we encounter an error */}
+//         {error ? <p>{error.message}</p> : null}
+//         {/* // Here's our data check */}
+//         {!isLoading ? (
+//           users.map((user) => {
+//             const { username, name, email } = user;
+//             return (
+//               <div key={username}>
+//                 <p>Name: {name}</p>
+//                 <p>Email Address: {email}</p>
+//                 <hr />
+//               </div>
+//             );
+//           })
+//         ) : (
+//           // If there is a delay in data, let's let the user know it's loading
+//           <h3>Loading...</h3>
+//         )}
+//       </>
+//     );
+//   }
+// }
